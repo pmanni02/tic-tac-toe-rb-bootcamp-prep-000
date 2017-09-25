cec972230573f931c084f546e46574b272c192d1
Phoebe Manning <phoebe.manning8@gmail.com>
2017-09-25 20:49:26 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,124 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end
+
+def position_taken?(board, location)
+  board[location] != " " && board[location] != ""
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def turn_count(board)
+  i = 0
+  counter = 0
+  while i<10
+    if board[i] != " "
+      counter += 1
+    end
+    i+=1
+  end
+  return 
+end
+# Define your play method below
+def play(board)
+  counter = 0
+  loop do
+    turn(board)
+    counter += 1
+    if counter === 9
+      break
+    end
+  end
+end
+
+# Helper Method
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |comb|
+    #puts comb.display
+    win_comb = comb
+    if(board[comb[0]]===board[comb[1]]&&board[comb[0]]===board[comb[2]])&&(board[comb[0]] != " ")
+      return win_comb
+    end
+  end
+  return false
+end
+
+def full?(board)
+  i = 0
+  full = true
+  while i < 10
+    if board[i] === " "
+      full = false
+    end
+    i += 1
+  end
+  return full
+end
+
+def draw?(board)
+  if (full?(board) && !won?(board))
+    return true
+  else
+    #board is not full
+    return false
+  end
+end
+
+def over?(board)
+  if won?(board) || draw?(board) || full?(board)
+    return true
+  else
+    return false
+  end
+end
+
+def winner(board)
+  if won?(board) === false
+    return nil
+  else
+    return board[won?(board)[0]]
+  end
+end
 
